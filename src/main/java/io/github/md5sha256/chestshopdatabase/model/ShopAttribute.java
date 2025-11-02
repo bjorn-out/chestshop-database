4f40d7ab51d3ab52b3ffcabdb4d493fe5d56e18b
Kacy Luzzardi <him@luzzardik.net>
2025-11-02 17:06:51 +0100

    Add UNIT_BUY_PRICE and UNIT_SELL_PRICE as shop attributes

--- src/main/java/io/github/md5sha256/chestshopdatabase/model/ShopAttribute.java
+++ src/main/java/io/github/md5sha256/chestshopdatabase/model/ShopAttribute.java
@@ -5,6 +5,8 @@ import javax.annotation.Nonnull;
 public enum ShopAttribute {
     BUY_PRICE("Buy Price"),
     SELL_PRICE("Sell Price"),
+    UNIT_BUY_PRICE("Unit Buy Price"),
+    UNIT_SELL_PRICE("Unit Sell Price"),
     STOCK("Stock"),
     REMAINING_CAPACITY("Remaining Capacity"),
     QUANTITY("Quantity"),
