0b4e88c20e7540c7923d915c0bbac45818011587
Kacy Luzzardi <him@luzzardik.net>
2025-11-05 22:08:40 +0100

    Add Shop#regionName & HydratedShop#regionName (fetching from WG API)

--- src/main/java/io/github/md5sha256/chestshopdatabase/model/Shop.java
+++ src/main/java/io/github/md5sha256/chestshopdatabase/model/Shop.java
@@ -1,9 +1,21 @@
 package io.github.md5sha256.chestshopdatabase.model;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import io.github.md5sha256.chestshopdatabase.util.BlockPosition;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.World;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.UUID;
 
 public record Shop(
@@ -48,5 +60,16 @@ public record Shop(
     public Double unitSellPrice () {
         if (sellPrice() == null) return null;
         return sellPrice() / quantity();
+    }
+
+    public String regionName () {
+        RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        World world = Bukkit.getServer().getWorld(worldId());
+        if (world == null) return "N/A";
+        RegionManager manager = container.get(BukkitAdapter.adapt(world));
+        if (manager == null) return "N/A";
+        ApplicableRegionSet regions = manager.getApplicableRegions(BukkitAdapter.adapt(new Location(world, posX(), posY(), posZ())).toVector().toBlockPoint());
+        Optional<ProtectedRegion> firstRegionInPriority = regions.getRegions().stream().min(Comparator.comparingInt(ProtectedRegion::getPriority));
+        return firstRegionInPriority.isEmpty() ? "N/A" : firstRegionInPriority.get().getId();
     }
 }
